221671a2346ac12595ff4afe50c795997a94b6f2
Bazsi <ultraxon02@gmail.com>
2022-05-06 19:02:55 +0100

    const XX YY

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "graphics.hpp"
 using namespace genv;
-
+const int XX=800;
+const int YY=600;
 //modositas
+
 int main()
 {
-    gout.open(400,400);
+    gout.open(XX,YY);
     gout << font("LiberationSans-Regular.ttf",20);
     gout <<text("hello world")<< refresh;
     event ev;
